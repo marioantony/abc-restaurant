a86679b92d9826ded735f578f51038542c75533a
Mario Antony <marioantony@Marios-MacBook-Pro.local>
2024-09-09 16:02:41 +0530

    1st commit

---
+++ abc-backend/server.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
